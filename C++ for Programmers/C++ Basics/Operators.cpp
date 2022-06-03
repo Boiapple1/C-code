2013c044748b7675914824e1b220e243ad61ef6f
Oscar Manzano <manzano.a1996@gmail.com>
2022-06-02 20:15:04 -0500

    update

---
+++ C++ for Programmers/C++ Basics/Operators.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Arithmetic operators are used to perform numeric operations on operands.
+	//int values
+	
+	// Initilaize operand1 and operand2
+	int operand1 = 50;
+	int operand2 = 26;
+	// Prints value of operand1 and operand2
+	cout << "Values of operands are:" << endl;
+	cout << "operand1 = " << operand1 << " , operand2 = " << operand2 << endl;
+	// Adds operand1 and operand2; and print their result
+	cout << "Addition = " << operand1 + operand2 << endl;
+	// Subtracts operand1 and operand2; and print their result
+	cout << "Subtraction = " << operand1 - operand2 << endl;
+	// Multiplies operand1 and operand2; and print their result
+	cout << "Multiplication = " << operand1 * operand2 << endl;
+	// Divides operand1 and operand2; and print their result
+	cout << "Division = " << operand1 / operand2 << endl;
+	// Returns remainder of operand1 and operand2; and print it
+	cout << "Modulus = " << operand1 % operand2 << endl;
+	
+	//float values
+	 // Initilaize operand1 and operand2
+	float operand1 = 50.0;
+	float operand2 = 26.0;
+	// Prints value of operand1 and operand2
+	cout << "Values of operands are:" << endl;
+	cout << "operand1 = " << operand1 << " , operand2 = " << operand2 << endl;
+	// Adds operand1 and operand2; and print their result
+	cout << "Addition = " << operand1 + operand2 << endl;
+	// Subtracts operand1 and operand2; and print their result
+	cout << "Subtraction = " << operand1 - operand2 << endl;
+	// Multiplies operand1 and operand2; and print their result
+	cout << "Multiplication = " << operand1 * operand2 << endl;
+	// Divides operand1 and operand2; and print their result
+	cout << "Division = " << operand1 / operand2 << endl;
+	// Returns remainder of operand1 and operand2; and print it
+	//cout << "Modulus = " << operand1 % operand2 << endl;
+	
+
+
+
+}
