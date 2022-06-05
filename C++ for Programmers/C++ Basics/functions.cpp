71a835a5925b7237082aa8eae271fc6589434ca9
Oscar Manzano <manzano.a1996@gmail.com>
2022-06-05 12:23:27 -0500

    update

--- C++ for Programmers/C++ Basics/functions.cpp
+++ C++ for Programmers/C++ Basics/functions.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
+
+//global variables
+int global = 3;
+
+void function() {
+    int function_local = 10;
+    cout << "global = " << global << endl;
+    global = 9;
+}
+
+//function overloading
+//meaning same name different parameters 
+const char* min(const char* s, const char* t) {
+    return (strcmp(s, t) < 0) ? s : t;
+}
+
+float min(float x, float y) {
+    return (x < y) ? x : y;
+}
+
+float min(float x, float y, float z) {
+    return x < y ? (x < z ? x : z) : (y < z ? y : z);
+}
+
+
 
 // Function definition
 int make_juice(int water = 1, int fruit = 3) {
@@ -24,6 +50,9 @@ void passReference(int& number) {
     number = number * 10;
     cout << "Value of number inside the function = " << number << endl;
 }
+
+//lambda
+int addFunc(int a, int b) { return a + b; }
 
 
 int main() {
@@ -56,8 +85,44 @@ int main() {
     passReference(number);
     cout << "Value of number after function call = " << number << endl;
 
+    //global variables
+    int main_local = 20;
+    global = 5;
+    cout << "global = " << global << endl;
+    global = 7;
 
+    function();
+    cout << "global = " << global << endl;
 
+    //overloading
+    const char* s = min("abc", "xyz");
+    float f = min(4.45F, 1.23f);
+    int f2 = min(2011, 2014);
+    // float f3 = min("abc", 1.23f);
+
+    std::cout << s << std::endl;
+    std::cout << f << std::endl;
+    std::cout << f2 << std::endl;
+
+    float f2 = min(4.45f, 1.23f, 0.19f);
+
+    std::cout << f << std::endl;
+    std::cout << f2 << std::endl;
+
+    /*lambda functions
+    [] no binding
+    [a] a per copy
+    [&a] a per reference
+    [=] all used variables per copy
+    [&] all used variable per reference
+    [=,&a] per default per copy,  a per reference
+    [this] data and member of the enclosing class per copy
+    [&,a] per default per refence; a per copy
+    [1=std::move(lock)] moves lock (c++14)
+    */
+    auto addObj = [](int a, int b) { return a + b; };
+
+    addObj(3, 4) == addFunc(3, 4);
 
     return 0;
 }
