10626b25fee60b0f240bb2cad658dc167edf63e9
Oscar Manzano <manzano.a1996@gmail.com>
2022-06-07 16:07:15 -0500

    update

--- C++ for Programmers/C++ Basics/Structure.cpp
+++ C++ for Programmers/C++ Basics/Structure.cpp
@@ -7,6 +7,26 @@ struct Student {
 	int roll_number;
 	int marks;
 };
+
+// function fillStudent
+Student fillStudent(string name, int roll_number, int marks) {
+	Student s;
+	s.name = name;
+	s.roll_number = roll_number;
+	s.marks = marks;
+	return s;
+}
+
+//creating a function for printing the struct info
+
+void printStudent(Student s) {
+	cout << "Student information" << endl;
+	cout << "Name = " << s.name << endl;
+	cout << "Roll Number = " << s.roll_number << endl;
+	cout << "Marks = " << s.marks << endl;
+}
+
+
 // main function
 int main() {
 	Student s1, s2, s3;
@@ -56,6 +76,48 @@ int main() {
 	cout << "Name = " << s[1].name << endl;
 	cout << "Roll Number = " << s[1].roll_number << endl;
 	cout << "Marks = " << s[1].marks << endl;
+
+	printStudent(s[0]);
+	printStudent(s[1]);
+
+	//return a structure from a function
+
+	s[0] = fillStudent("John", 1, 50);
+	printStudent(s[0]);
+
+	s[1] = fillStudent("Alice", 2, 43);
+	printStudent(s[1]);
+
+
+	//structure pointers
+	 // Declare structure variable
+	struct Student s1;
+	// Declare structure pointer
+	struct Student* ptrs1;
+	// Store address of structure variable in structure pointer
+	ptrs1 = &s1;
+
+	// Set value of name
+	(*ptrs1).name = "John";
+	// Set value of roll_number
+	(*ptrs1).roll_number = 1;
+	// Set value of marks
+	(*ptrs1).marks = 50;
+
+	//you can also use the arrow
+	// Set value of name
+	ptrs1->name = "John";
+	// Set value of roll_number
+	ptrs1->roll_number = 1;
+	// Set value of marks
+	ptrs1->marks = 50;
+
+	// Print value of structure member
+	cout << "s1 Information:" << endl;
+	cout << "Name = " << (*ptrs1).name << endl;
+	cout << "Roll Number = " << (*ptrs1).roll_number << endl;
+	cout << "Marks = " << (*ptrs1).marks << endl;
+
 
 	return 0;
 }
