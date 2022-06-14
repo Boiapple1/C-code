120b6e69a0e297e7c80eec70d9aba4c2a8034e9c
Oscar Manzano <manzano.a1996@gmail.com>
2022-06-14 07:12:28 -0500

    update

---
+++ C++ for Programmers/C++ OOP/challenge7.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+protected:
+	float balance;
+public:
+	Account() {
+		balance = 0;
+	}
+	Account(float balan) {
+		balance = balan;
+	}
+	virtual void Withdraw(float amount) {}
+	virtual void Deposit(float amount){}
+	virtual void printBalance(){}
+};
+class Savings : public Account {
+	float interest_rate=.8;
+
+public:
+	Savings( float B) :Account(B) {}
+
+	void Deposit(float amount) {
+		balance += amount + (amount * interest_rate);
+	}
+
+	void Withdraw(float amount) {
+		if (balance > 0)
+			balance -= amount + (amount * interest_rate);
+		else
+			cout << "Sorry not money\n";
+	}
+	void printBalance() {
+		cout << "Balance in your saving account: " << balance << endl;
+	}
+};
+
+class Current : public Account {
+public:
+	Current(int bal) :Account(bal) {}
+	void Deposit(float amount) {
+		balance += amount;
+	}
+	void Withdraw(float amount) {
+		if (balance > 0)
+			balance -= amount;
+		else
+			cout << "Sorry not money\n";
+	
+	}
+	void printBalance() {
+		cout << "Balance in your current account: " << balance << endl;
+	}
+};
+
+int main() {
+	// creating savings account object
+	Savings s1(50000);
+	Account* acc = &s1;   // pointing acc to savings object//
+	acc->Deposit(1000);
+	acc->printBalance();
+
+	acc->Withdraw(3000);
+	acc->printBalance();
+
+	cout << endl;
+
+	// creating current account object
+	Current c1(50000);
+	acc = &c1;     //// pointing acc to current object
+	acc->Deposit(1000);
+	acc->printBalance();
+
+	acc->Withdraw(3000);
+	acc->printBalance();
+}
