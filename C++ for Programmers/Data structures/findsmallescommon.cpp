9d80cc9544252acff7230b17ba11ad2c59078db5
Oscar Manzano <manzano.a1996@gmail.com>
2022-06-22 14:37:12 -0500

    update

---
+++ C++ for Programmers/Data structures/findsmallescommon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int FIndLeastCommonNumber(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
+	//initialize starting indexes for arr1, arr2 and arr3
+	int i = 0, j = 0, k = 0;
+
+	while (i < arr1.size() && j < arr2.size() && k < arr3.size()) {
+		//finding the smallest common number
+	}
+}
